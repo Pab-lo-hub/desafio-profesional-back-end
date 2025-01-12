0b92cce348e70aad9fc443f94c741d90b9609dcc
Pablo S <salaberry.pablo@gmail.com>
2025-01-12 10:27:50 -0300

    Initial setup

---
+++ src/main/java/com/desafio_profesional/demo/DesafioProfesionalApplication.java
@@ -0,0 +1,13 @@
+package com.desafio_profesional.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioProfesionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioProfesionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio_profesional/demo/DesafioProfesionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio_profesional.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioProfesionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
